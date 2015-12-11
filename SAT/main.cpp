8cacdf6bf1ae5bb7e33f8e6fbda201157e8c1d5b
Jindřich Štěpánek <stepanek@avast.com>
2015-12-12 00:04:44 +0100

    SAT - evaluation

--- SAT/main.cpp
+++ SAT/main.cpp
@@ -10,17 +10,29 @@
 #include <iostream>
 #include <vector>
 #include <sstream>
+#include <algorithm>
 
-#define DEBUG true
+#define DEBUG false
 
 using namespace std;
+
+bool contains(const vector<string> & v, string item) {
+    if (v.empty())
+        return false;
+    
+    return (std::find(v.begin(), v.end(), item) != v.end());
+}
 
 bool is_operator(const string & s) {
     return (s == "+" || s == "." || s == "!");
 }
 
+bool is_symbol(char c) {
+    return c == '+' || c == '.' || c == '!' || c == '(' || c == ')';
+}
+
 bool is_element(const string & s) {
-    return !is_operator(s) && !(s == "(") && !(s == ")");
+    return s.size() > 1 || !is_symbol(s[0]);
 }
 
 int priority(const string & op) {
@@ -40,7 +52,6 @@ void print(vector<string> * v) {
         cout << v->at(i) << " ";
     }
     cout << endl;
-
 }
 
 vector<string> * conjunctions_list(string s) {
@@ -87,7 +98,7 @@ string conjunction(string a, string b) {
     if (b == "")
         return a;
 
-    // split to list of conjunctions which will be multiplied
+    // split to lists of conjunctions which will be multiplied
     
     vector<string> * conjunctions1 = conjunctions_list(a);
     vector<string> * conjunctions2 = conjunctions_list(b);
@@ -154,22 +165,52 @@ string negation(string s) {
     }
     
     if (DEBUG) {
-        cout << "negate " << s << " -> " << result << endl;
+        cout << "negation " << s << " -> " << result << endl;
     }
     
     return result;
 }
 
+string read_token(string s, int start, int & end) {
+    int i = start;
+    int size = s.size();
+    
+    string res = "";
+    
+    while (true) {
+        if (is_symbol(s[i]) && res == "") {
+            res += s[i];
+            end = i;
+            return res;
+        }
+        
+        if (i >= size || is_symbol(s[i])) {
+            end = i-1;
+            return res;
+        }
+        
+        if (s[i] != ' ')
+            res += s[i];
+        
+        i++;
+    }
+}
+
 vector<string> * build_postfix(const string & expression) {
-    istringstream iss(expression);
     vector<string> tokens;
     
-    while (!iss.eof())
-    {
-        string temp;
-        iss >> temp;
+    int position = 0;
+    int end;
+    int size = expression.size();
+    
+    while (true) {
+        string token = read_token(expression, position, end);
+        position = end + 1;
         
-        tokens.push_back(temp);
+        tokens.push_back(token);
+
+        if (position >= size)
+            break;
     }
     
     vector<string> * postfix = new vector<string>;
@@ -211,24 +252,23 @@ vector<string> * build_postfix(const string & expression) {
         
     }
     
-    //pop remaining operators
     while(!stack.empty())
     {
         postfix->push_back(stack.back());
         stack.pop_back();
     }
     
-    //tree = reverse_vector(tree);
-    
     return postfix;
 }
 
-string parse_postfix(vector<string> * postfix) {
+string parse_dnf(vector<string> * postfix) {
     vector<string> stack;
     
     for (long i = 0; i < postfix->size(); i++) {
         string token = postfix->at(i);
-        cout << "token " << token << endl;
+        
+        if (DEBUG)
+            cout << "token " << token << endl;
         
         if (token == "!") {
             string op = stack.back();
@@ -263,15 +303,69 @@ string parse_postfix(vector<string> * postfix) {
     return stack.back();
 }
 
+bool evaluate_conjunction(string s) {
+    string element = "";
+    long size = s.size();
+    
+    vector<string> positive;
+    vector<string> negative;
+    
+    for (long i = 0; i < size; i++) {
+        if (s[i] == '.' || i == size - 1) {
+            if (element[0] == '!') {
+                element = element.substr(1, element.size() - 1);
+                
+                if (contains(positive, element))
+                    return false;
+                
+                negative.push_back(element);
+            } else {
+                if (contains(negative, element))
+                    return false;
+                
+                positive.push_back(element);
+            }
+            
+            element = "";
+        } else {
+            element += s[i];
+        }
+    }
+    
+    return true;
+}
+
+bool SAT(const string & s) {
+    cout << s << endl;
+    
+    vector<string> * postfix = build_postfix(s);
+    print(postfix);
+    
+    string dnf = parse_dnf(postfix);
+    cout << dnf << endl;
+    cout << "---" << endl;
+
+    bool res = true;
+    
+    vector<string> * conjuntions = conjunctions_list(dnf);
+    
+    for (long i = 0; i < conjuntions->size(); i++) {
+        if (!evaluate_conjunction(conjuntions->at(i))) {
+            res = false;
+            break;
+        }
+    }
+    
+    string str = res ? "TRUE" : "FALSE";
+    cout << str << endl << "---" << endl;
+
+    return true;
+}
+
 int main(int args, char ** argv) {
-    string a = "a . ! b + ! ( ( c . d ) + u . v ) + ! e . f";
+    SAT("a . ! b + ! ( ( c . d . ! d ) + u . v ) + ! e . f");
     
-    vector<string> * tree = build_postfix(a);
-    print(tree);
-    
-    string res = parse_postfix(tree);
-    
-    cout << "---" << endl << res << endl << "---" << endl;
+    SAT("aaa.( bbb + ccc ).!bbb");
     
     return 0;
 }
